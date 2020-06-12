6cfa2fc8db697114eb9b6a6e0b498c3f4e859aa2
Adrian Beściak <adrian.besciak99@gmail.com>
2020-06-12 13:05:08 +0200

    renamed variable names in decorator's main

--- java/decorator/Main.java
+++ java/decorator/Main.java
@@ -2,18 +2,18 @@ package decorator;
 
 public class Main {
     public static void main(String[] args) {
-        String salaryRecords = "Ala ma kota";
-        DataSourceDecorator encoded = new EncryptionDecorator(
+        String text = "Ala ma kota";
+        DataSourceDecorator encodedAndCompressed = new EncryptionDecorator(
                 new CompressionDecorator(
-                        new FileDataSource("out/OutputDemo.txt")));
-        encoded.writeData(salaryRecords);
-        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+                        new FileDataSource("out/compressedFile.txt")));
+        encodedAndCompressed.writeData(text);
+        DataSource justRead = new FileDataSource("out/compressedFile.txt");
 
-        System.out.println("- Input ----------------");
-        System.out.println(salaryRecords);
-        System.out.println("- Encoded --------------");
-        System.out.println(plain.readData());
+        System.out.println("- String ----------------");
+        System.out.println(text);
+        System.out.println("- Encoded and compressed --------------");
+        System.out.println(justRead.readData());
         System.out.println("- Decoded --------------");
-        System.out.println(encoded.readData());
+        System.out.println(encodedAndCompressed.readData());
     }
 }
